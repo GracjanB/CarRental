53a480ba88c35eaa3f4db5d1df562748fa93ac17
Paweł Bucior <pawel121111@gmail.com>
2020-04-04 23:14:01 +0200

    Mailingi

--- Backend/src/main/java/pl/bucior/carrental/service/event/RegistrationListener.java
+++ Backend/src/main/java/pl/bucior/carrental/service/event/RegistrationListener.java
@@ -44,7 +44,7 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipientAddress);
         email.setSubject(subject);
-        email.setText(message + "\r\n" + "http://localhost:8080" + confirmationUrl);
+        email.setText(message + "\r\n" + "URL +" + confirmationUrl);
         try {
             javaMailSender.send(email);
             accountVerificationTokenService.changeStatus(token);
