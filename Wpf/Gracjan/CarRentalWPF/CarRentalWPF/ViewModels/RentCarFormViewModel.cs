2a4229c6532f6190e903a9cf0996527e1da44654
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-04-05 22:03:50 +0200

    Mały refactoring kodu.

---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/RentCarFormViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalWPF.ViewModels
+{
+    public class RentCarFormViewModel : Screen
+    {
+        public BindableCollection<int> Days { get; set; }
+
+        public BindableCollection<string> Months { get; set; }
+
+        public BindableCollection<int> Years { get; set; }
+
+        public RentCarFormViewModel()
+        {
+            LoadDays();
+            LoadMonths();
+            LoadYears();
+
+        }
+
+        private void LoadDays()
+        {
+            Days = new BindableCollection<int>();
+            for (int i = 1; i < 32; i++)
+                Days.Add(i);
+        }
+
+        private void LoadMonths()
+        {
+            Months = new BindableCollection<string>();
+            Months.Add("Styczeń");
+            Months.Add("Luty");
+            Months.Add("Marzec");
+            Months.Add("Kwiecień");
+            Months.Add("Maj");
+            Months.Add("Czerwiec");
+            Months.Add("Lipiec");
+            Months.Add("Sierpień");
+            Months.Add("Wrzesień");
+            Months.Add("Październik");
+            Months.Add("Listopad");
+            Months.Add("Grudzień");
+        }
+
+        private void LoadYears()
+        {
+            Years = new BindableCollection<int>();
+            for (int i = 1901; i < 2003; i++)
+                Years.Add(i);
+        }
+    }
+}
