7cb32f5f7e075320f38aa04033f11814819732bc
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-04-05 22:01:16 +0200

    Dodano widok logowania.

---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using CarRentalWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalWPF.Validators;
+using System.Windows;
+
+namespace CarRentalWPF.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        public LoginModel loginModel { get; set; }
+
+        private LoginFormValidator _loginValidator { get; set; }
+
+        public LoginViewModel()
+        {
+            loginModel = new LoginModel();
+            _loginValidator = new LoginFormValidator();
+        }
+
+        public void LoginButton()
+        {
+            var result = _loginValidator.Validate(loginModel);
+
+            if (result.IsValid)
+            {
+                // TODO: Login user
+
+                MessageBox.Show("Login good.");
+            }
+            else
+            {
+                string resultString = "";
+
+                foreach (var failure in result.Errors)
+                    resultString += "Property: " + failure.PropertyName + " - Error: " + failure.ErrorMessage + "\n";
+
+                MessageBox.Show(resultString);
+            }
+                
+        }
+    }
+}
