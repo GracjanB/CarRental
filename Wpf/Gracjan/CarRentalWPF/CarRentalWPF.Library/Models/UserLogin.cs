242586e13ef0f2d0ebdf7be7dbd29a17b74fd8da
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-04-08 18:42:28 +0200

    Added Rest Client with basic login operations.

---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF.Library/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalWPF.Library.Models
+{
+    public class UserLogin
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF.Library/RequestsContentModels/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalWPF.Library.RequestsContentModels
+{
+    public class UserInfo
+    {
+        public User user { get; set; }
+
+        public bool isSucceded { get; set; }
+
+        public string message { get; set; }
+    }
+}
