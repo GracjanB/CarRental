81d063aed1308f733cecba50e4970e626a7da085
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-11 16:00:52 +0200

    Added view for display employee details

---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageEmployeeDetailsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using CarRentalWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalWPF.ViewModels
+{
+    public class AgencyManageEmployeeDetailsViewModel : Screen
+    {
+        private SimpleContainer _container { get; set; }
+
+        public AgencyManageEmployeeDetailsViewModel(SimpleContainer simpleContainer)
+        {
+            _container = simpleContainer;
+        }
+
+        public void LoadModel(EmployeeModel employeeModel)
+        {
+            Employee = employeeModel;
+        }
+
+        #region Employee Model
+
+        private EmployeeModel _employee;
+
+        public EmployeeModel Employee
+        {
+            get { return _employee; }
+            set { _employee = value; }
+        }
+
+        #endregion
+
+
+        #region PopUp Menu
+
+        public void EditEmployee()
+        {
+            // TODO: Show employee edit screen
+        }
+
+        public void MoveBack()
+        {
+            var agencyManageEmployeesVM = _container.GetInstance<AgencyManageEmployeesViewModel>();
+            var conductorObject = (AgencyManageViewModel) this.Parent;
+            conductorObject.ActivateItem(agencyManageEmployeesVM);
+        }
+
+        #endregion
+    }
+}
