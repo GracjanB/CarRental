985be06ec47f47054152e3399238f3c87665da78
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-12 14:26:56 +0200

    Wired up EmployeeEdit Form validation results with ViewModel.

--- Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageEmployeeEditViewModel.cs
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageEmployeeEditViewModel.cs
@@ -38,6 +38,7 @@ namespace CarRentalWPF.ViewModels
 
         #endregion
 
+
         #region Employee Model
 
         private EmployeeModel _employee;
@@ -46,6 +47,163 @@ namespace CarRentalWPF.ViewModels
         {
             get { return _employee; }
             set { _employee = value; }
+        }
+
+        #endregion
+
+
+        #region Validation Result
+
+        private bool _firstNameValidationResult;
+        private bool _lastNameValidationResult;
+        private bool _peselValidationResult;
+        private bool _idCardNumberValidationResult;
+        private bool _streetValidationResult;
+        private bool _buildingNoValidationResult;
+        private bool _flatNoValidationResult;
+        private bool _postalCodeValidationResult;
+        private bool _cityValidationResult;
+        private bool _emailValidationResult;
+        private bool _phoneNumberValidationResult;
+        private bool _roleValidationResult;
+
+        public bool FirstNameValidationResult
+        {
+            get { return _firstNameValidationResult; }
+            set
+            {
+                _firstNameValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool LastNameValidationResult
+        {
+            get { return _lastNameValidationResult; }
+            set 
+            { 
+                _lastNameValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PESELValidationResult
+        {
+            get { return _peselValidationResult; }
+            set 
+            { 
+                _peselValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool IdCardNumberValidationResult
+        {
+            get { return _idCardNumberValidationResult; }
+            set 
+            { 
+                _idCardNumberValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool StreetValidationResult
+        {
+            get { return _streetValidationResult; }
+            set 
+            {
+                _streetValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool BuildingNoValidationResult
+        {
+            get { return _buildingNoValidationResult; }
+            set 
+            { 
+                _buildingNoValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool FlatNoValidationResult
+        {
+            get { return _flatNoValidationResult; }
+            set 
+            { 
+                _flatNoValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PostalCodeValidationResult
+        {
+            get { return _postalCodeValidationResult; }
+            set 
+            { 
+                _postalCodeValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool CityValidationResult
+        {
+            get { return _cityValidationResult; }
+            set 
+            { 
+                _cityValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool EmailValidationResult
+        {
+            get { return _emailValidationResult; }
+            set 
+            { 
+                _emailValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PhoneNumberValidationResult
+        {
+            get { return _phoneNumberValidationResult; }
+            set 
+            { 
+                _phoneNumberValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool RoleValidationResult
+        {
+            get { return _roleValidationResult; }
+            set 
+            { 
+                _roleValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool OverallValidationResult
+        {
+            get
+            {
+                return FirstNameValidationResult &&
+                       LastNameValidationResult &&
+                       PESELValidationResult &&
+                       IdCardNumberValidationResult &&
+                       StreetValidationResult &&
+                       BuildingNoValidationResult &&
+                       FlatNoValidationResult &&
+                       PostalCodeValidationResult &&
+                       CityValidationResult &&
+                       EmailValidationResult &&
+                       PhoneNumberValidationResult &&
+                       RoleValidationResult;
+            }
         }
 
         #endregion
