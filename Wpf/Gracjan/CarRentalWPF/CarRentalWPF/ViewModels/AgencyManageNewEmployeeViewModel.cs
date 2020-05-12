c2848438342a70ad0fb50628b1e88ab98d002993
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-12 15:53:50 +0200

    Wired up NewEmployee Form validation results with ViewModel

--- Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageNewEmployeeViewModel.cs
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageNewEmployeeViewModel.cs
@@ -52,5 +52,151 @@ namespace CarRentalWPF.ViewModels
         }
 
         #endregion
+
+
+        #region Validation Results
+
+        private bool _firstNameValidationResult;
+        private bool _lastNameValidationResult;
+        private bool _peselValidationResult;
+        private bool _idCardNumberValidationResult;
+        private bool _streetValidationResult;
+        private bool _buildingNoValidationResult;
+        private bool _flatNoValidationResult;
+        private bool _postalCodeValidationResult;
+        private bool _cityValidationResult;
+        private bool _emailValidationResult;
+        private bool _phoneNumberValidationResult;
+
+        public bool FirstNameValidationResult
+        {
+            get { return _firstNameValidationResult; }
+            set
+            {
+                _firstNameValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool LastNameValidationResult
+        {
+            get { return _lastNameValidationResult; }
+            set
+            {
+                _lastNameValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PESELValidationResult
+        {
+            get { return _peselValidationResult; }
+            set
+            {
+                _peselValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool IdCardNumberValidationResult
+        {
+            get { return _idCardNumberValidationResult; }
+            set
+            {
+                _idCardNumberValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool StreetValidationResult
+        {
+            get { return _streetValidationResult; }
+            set
+            {
+                _streetValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool BuildingNoValidationResult
+        {
+            get { return _buildingNoValidationResult; }
+            set
+            {
+                _buildingNoValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool FlatNoValidationResult
+        {
+            get { return _flatNoValidationResult; }
+            set
+            {
+                _flatNoValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PostalCodeValidationResult
+        {
+            get { return _postalCodeValidationResult; }
+            set
+            {
+                _postalCodeValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool CityValidationResult
+        {
+            get { return _cityValidationResult; }
+            set
+            {
+                _cityValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool EmailValidationResult
+        {
+            get { return _emailValidationResult; }
+            set
+            {
+                _emailValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool PhoneNumberValidationResult
+        {
+            get { return _phoneNumberValidationResult; }
+            set
+            {
+                _phoneNumberValidationResult = value;
+                NotifyOfPropertyChange(() => OverallValidationResult);
+            }
+        }
+
+        public bool OverallValidationResult
+        {
+            get
+            {
+                return FirstNameValidationResult &&
+                       LastNameValidationResult &&
+                       PESELValidationResult &&
+                       IdCardNumberValidationResult &&
+                       StreetValidationResult &&
+                       BuildingNoValidationResult &&
+                       FlatNoValidationResult &&
+                       PostalCodeValidationResult &&
+                       CityValidationResult &&
+                       EmailValidationResult &&
+                       PhoneNumberValidationResult;
+            }
+        }
+
+
+        #endregion
     }
 }
