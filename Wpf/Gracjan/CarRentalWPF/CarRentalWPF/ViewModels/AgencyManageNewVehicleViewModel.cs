8e85ced51fe5f97e82ff77378c432fa6ae582822
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-13 13:04:50 +0200

    Implemented full template for adding new cars

--- Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageNewVehicleViewModel.cs
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/AgencyManageNewVehicleViewModel.cs
@@ -5,19 +5,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CarRentalWPF.ViewModels
 {
     public class AgencyManageNewVehicleViewModel : Screen
     {
 
+        public AgencyManageNewVehicleViewModel()
+        {
+            GenerateCarTypes();
+        }
+
+        #region On Initialize
+
+        private void GenerateCarTypes()
+        {
+            CarTypes = new BindableCollection<string>(Types);
+        }
+
+        #endregion
 
 
         #region Top Menu Buttons
 
+        public bool CanSaveCar
+        {
+            get
+            {
+                return !(MarkValidationResult &&
+                       ModelValidationResult &&
+                       EngineValidationResult &&
+                       PowerValidationResult &&
+                       MileageValidationResult &&
+                       PlateValidationResult &&
+                       VINValidationResult);
+            }
+        }
+
         public void SaveCar()
         {
-            // TODO: Make function to save car
+            if(!string.IsNullOrEmpty(SelectedCarType))
+            {
+                // Procedure to save new car
+            }
+            else
+            {
+                MessageBox.Show("Didn't chose any car type");
+            }
         }
 
         public void ClearForm()
@@ -63,6 +98,102 @@ namespace CarRentalWPF.ViewModels
                 NotifyOfPropertyChange(() => CarTypes);
             }
         }
+
+        #endregion
+
+
+        #region Validation Results
+
+        private bool _markValidationResult;
+        private bool _modelValidationResult;
+        private bool _engineValidationResult;
+        private bool _powerValidationResult;
+        private bool _mileageValidationResult;
+        private bool _plateValidationResult;
+        private bool _vinValidationResult;
+
+        public bool MarkValidationResult
+        {
+            get { return _markValidationResult; }
+            set
+            {
+                _markValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool ModelValidationResult
+        {
+            get { return _modelValidationResult; }
+            set
+            {
+                _modelValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool EngineValidationResult
+        {
+            get { return _engineValidationResult; }
+            set
+            {
+                _engineValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool PowerValidationResult
+        {
+            get { return _powerValidationResult; }
+            set
+            {
+                _powerValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool MileageValidationResult
+        {
+            get { return _mileageValidationResult; }
+            set
+            {
+                _mileageValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool PlateValidationResult
+        {
+            get { return _plateValidationResult; }
+            set
+            {
+                _plateValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        public bool VINValidationResult
+        {
+            get { return _vinValidationResult; }
+            set
+            {
+                _vinValidationResult = value;
+                NotifyOfPropertyChange(() => CanSaveCar);
+            }
+        }
+
+        #endregion
+
+
+        #region Resources
+
+        private readonly string[] Types = new string[]
+        {
+            "COMBI",
+            "VAN",
+            "SUV",
+            "SEDAN"
+        };
 
         #endregion
     }
