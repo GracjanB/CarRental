0cea06b9629a0a9f7bf9ee2ba8d418c4c7dc7e4c
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-15 16:50:11 +0200

    Added view for selecting rent date in car rental

---
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/ViewModels/RentCarFormDateChoiceViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalWPF.ViewModels
+{
+    public class RentCarFormDateChoiceViewModel : Screen
+    {
+        public DateTime SelectedDateFrom { get; set; } = DateTime.Today;
+
+        public DateTime SelectedTimeFrom { get; set; }
+
+        public DateTime SelectedDateTo { get; set; } = DateTime.Today;
+
+        public DateTime SelectedTimeTo { get; set; }
+
+        public string BasePrice { get; set; }
+
+        public void MoveBack()
+        {
+
+        }
+
+        public void MoveForward()
+        {
+
+        }
+    }
+}
