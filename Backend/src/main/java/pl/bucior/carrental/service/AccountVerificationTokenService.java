97783dbd9aa3e0b8ef36f80f3c96bb8f548699f2
Paweł Bucior <pawel121111@gmail.com>
2020-06-03 22:34:53 +0200

    Register fix

--- Backend/src/main/java/pl/bucior/carrental/service/AccountVerificationTokenService.java
+++ Backend/src/main/java/pl/bucior/carrental/service/AccountVerificationTokenService.java
@@ -1,9 +1,13 @@
 package pl.bucior.carrental.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import pl.bucior.carrental.configuration.exception.ErrorCode;
+import pl.bucior.carrental.configuration.exception.WsizException;
 import pl.bucior.carrental.model.jpa.AccountVerificationToken;
 import pl.bucior.carrental.repository.AccountVerificationTokenRepository;
+import pl.bucior.carrental.repository.UserRepository;
 
 import javax.transaction.Transactional;
 
@@ -13,6 +17,7 @@ import javax.transaction.Transactional;
 public class AccountVerificationTokenService {
 
     private final AccountVerificationTokenRepository accountVerificationTokenRepository;
+    private final UserRepository userRepository;
 
     public void createVerificationToken(Long userId, String token) {
         accountVerificationTokenRepository.save(AccountVerificationToken
@@ -20,6 +25,7 @@ public class AccountVerificationTokenService {
                 .token(token)
                 .mailSended(false)
                 .userId(userId)
+                .user(userRepository.findById(userId).orElseThrow(() -> new WsizException(HttpStatus.NOT_FOUND, ErrorCode.USER_NOT_FOUND)))
                 .build());
     }
 
