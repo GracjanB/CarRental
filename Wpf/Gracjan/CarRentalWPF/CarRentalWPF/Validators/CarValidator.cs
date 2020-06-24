154b2dda4ac5759f0cd570525092be90feb99f18
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-06-24 18:37:24 +0200

    Added car validators

--- Wpf/Gracjan/CarRentalWPF/CarRentalWPF/Validators/CarValidator.cs
+++ Wpf/Gracjan/CarRentalWPF/CarRentalWPF/Validators/CarValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows.Controls;
 
 namespace CarRentalWPF.Validators
@@ -10,9 +11,17 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Mark Validator
+            if (value != null)
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                    return new ValidationResult(false, "Field is required.");
 
-            return ValidationResult.ValidResult;
+                if (value.ToString().Length < 3 || value.ToString().Length > 30)
+                    return new ValidationResult(false, "Mark between 3 and 30.");
+
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -23,22 +32,17 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Model Validator
-
-            return ValidationResult.ValidResult;
-        }
-    }
-
-    /// <summary>
-    /// CarModel - Type Property
-    /// </summary>
-    public class CarTypeValidator : ValidationRule
-    {
-        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
-        {
-            // TODO: Make Type Validator
-
-            return ValidationResult.ValidResult;
+            if (value != null)
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                    return new ValidationResult(false, "Field is required.");
+                
+                if (value.ToString().Length < 3 || value.ToString().Length > 30)
+                    return new ValidationResult(false, "Model between 3 and 30.");
+                
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -49,9 +53,25 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Engine Validator
+            if (value != null)
+            {
+                int engine;
 
-            return ValidationResult.ValidResult;
+                try
+                {
+                    engine = Convert.ToInt32(value);
+                }
+                catch(Exception)
+                {
+                    return new ValidationResult(false, "Engine should be a number");
+                }
+
+                if (engine < 100 || engine > 10000)
+                    return new ValidationResult(false, "Engine between 100 and 10000");
+
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -62,9 +82,25 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Power Validator
+            if (value != null)
+            {
+                int power;
 
-            return ValidationResult.ValidResult;
+                try
+                {
+                    power = Convert.ToInt32(value);
+                }
+                catch (Exception)
+                {
+                    return new ValidationResult(false, "Power should be a number");
+                }
+
+                if (power < 5 || power > 1500)
+                    return new ValidationResult(false, "Power between 5 and 1500");
+
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -75,22 +111,25 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Mileage Validator
+            if (value != null)
+            {
+                int mileage;
 
-            return ValidationResult.ValidResult;
-        }
-    }
+                try
+                {
+                    mileage = Convert.ToInt32(value);
+                }
+                catch (Exception)
+                {
+                    return new ValidationResult(false, "Mileage should be a number");
+                }
 
-    /// <summary>
-    /// CarModel - Status Property
-    /// </summary>
-    public class CarStatusValidator : ValidationRule
-    {
-        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
-        {
-            // TODO: Make Status Validator
+                if (mileage < 1)
+                    return new ValidationResult(false, "Mileage cannot be less than 1");
 
-            return ValidationResult.ValidResult;
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -101,9 +140,17 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make Plate Validator
+            if (value != null)
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                    return new ValidationResult(false, "Field is required.");
 
-            return ValidationResult.ValidResult;
+                if (value.ToString().Length < 6 || value.ToString().Length > 20)
+                    return new ValidationResult(false, "Plate between 5 and 20.");
+
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 
@@ -114,9 +161,17 @@ namespace CarRentalWPF.Validators
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            // TODO: Make VIN Validator
+            if (value != null)
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                    return new ValidationResult(false, "Field is required.");
 
-            return ValidationResult.ValidResult;
+                if (value.ToString().Length != 17)
+                    return new ValidationResult(false, "VIN is incorrect");
+
+                return ValidationResult.ValidResult;
+            }
+            else return new ValidationResult(false, "");
         }
     }
 }
